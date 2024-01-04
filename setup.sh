79cba051650fdef00d5fe42cdb3ec906b8650b46
Lucas Rodrigues <lucasrodriguespimentel@gmail.com>
2024-01-04 04:37:18 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-wget https://raw.githubusercontent.com/lucasrdrgs/ts-eslint-setup/main/.eslintrc.json -O .eslintrc.json;
-wget https://raw.githubusercontent.com/lucasrdrgs/ts-eslint-setup/main/.prettierignore -O .prettierignore;
-wget https://raw.githubusercontent.com/lucasrdrgs/ts-eslint-setup/main/.prettierrc -O .prettierrc;
-wget https://raw.githubusercontent.com/lucasrdrgs/ts-eslint-setup/main/tsconfig.json -O tsconfig.json;
-npm i -D typescript ts-node prettier eslint eslint-plugin-prettier eslint-config-prettier @typescript-eslint/parser @typescript-eslint/eslint-plugin @darraghor/eslint-plugin-nestjs-typed > /dev/null;
+wget -q https://raw.githubusercontent.com/lucasrdrgs/ts-eslint-setup/main/.eslintrc.json -O .eslintrc.json;
+wget -q https://raw.githubusercontent.com/lucasrdrgs/ts-eslint-setup/main/.prettierignore -O .prettierignore;
+wget -q https://raw.githubusercontent.com/lucasrdrgs/ts-eslint-setup/main/.prettierrc -O .prettierrc;
+wget -q https://raw.githubusercontent.com/lucasrdrgs/ts-eslint-setup/main/tsconfig.json -O tsconfig.json;
+npm i --quiet -D typescript ts-node prettier eslint eslint-plugin-prettier eslint-config-prettier @typescript-eslint/parser @typescript-eslint/eslint-plugin @darraghor/eslint-plugin-nestjs-typed;
 echo "Successfully set up TS + ESLint + Prettier based off of NestJS's linting style.";
